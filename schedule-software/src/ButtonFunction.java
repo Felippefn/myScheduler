e290c19775fdfdfb39b0e4adccf32c6543d2906d
Felippe Nagy <felippenagy@outlook.com>
2022-06-18 21:27:52 -0300

    feat: :fireworks: added new function to get day week string

--- schedule-software/src/ButtonFunction.java
+++ schedule-software/src/ButtonFunction.java
@@ -1,7 +1,14 @@
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class ButtonFunction {
+
+    public static String getWeekDayString() {
+        Date date = new Date();
+        DateFormat formatter = new SimpleDateFormat("EEEE");
+        return formatter.format(date);
+    }
 
     public static String getActualData() {
         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
