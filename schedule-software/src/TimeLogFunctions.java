cb1d15eeb2e81c0111969a9522a3e413f3b98075
Felippe Nagy <felippenagy@outlook.com>
2022-06-18 21:36:44 -0300

    fix: :wrench: fixed name to understanding

--- schedule-software/src/TimeLogFunctions.java
+++ schedule-software/src/TimeLogFunctions.java
@@ -2,7 +2,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class ButtonFunction {
+public class TimeLogFunctions {
 
     public static String getWeekDayString() {
         Date date = new Date();
@@ -14,11 +14,6 @@ public class ButtonFunction {
         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         Date date = new Date();
         return formatter.format(date);
-    }
-
-    public static String returnValueClick() {
-        String actualTime = TestTime.getActualData();
-        return actualTime;
     }
 
 }
