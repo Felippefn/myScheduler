719074f35427e17ead7dc2d8a4e8bf9c96436da1
Felippe Nagy <79763393+Felippefn@users.noreply.github.com>
2022-06-20 11:36:30 -0300

    Deleting files used for test

--- schedule-software/src/TestingFunctions.java
+++
@@ -1,7 +0,0 @@
-public class TestingFunctions {
-    public static void main(String[] args) {
-        System.out.println(CreatePayload.returnCorrectPayload(TimeLogFunctions.getCurrentData(), "saturday", "Java", "2"));
-        System.out.println(TimeLogFunctions.getWeekDayString());
-    }
-
-}
